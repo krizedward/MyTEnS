9375d6536f48144cf80c048500bbca0c3055a4b4
krizedward <edward.mangare@binus.ac.id>
2022-08-23 09:55:57 +0700

    v.1.4 - frontend web

--- src/components/todo-create/TodoCreate.js
+++ src/components/todo-create/TodoCreate.js
@@ -1,10 +1,31 @@
+import { useState } from 'react'
 import './TodoCreate.css'
 
-const TodoCreate = () => {
+const TodoCreate = (props) => {
+    const [getInputTodo, setInputTodo] = useState('')
     
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        const newData = {
+            id: Math.floor(Math.random() * 100) +1,
+            title: getInputTodo
+        }
+
+        props.onCrateTodo(newData)
+
+        setInputTodo('')
+
+        // console.log(newData)
+    }
+
+    const handleInputTodo = (event) => {
+        setInputTodo(event.target.value)
+        console.log(getInputTodo)
+    }
+
     return (
-        <form className="todo-form">
-            <input type="text" placeholder="akun GitHub.."></input>
+        <form className="todo-form" onSubmit = {handleSubmit}>
+            <input type="text" value={getInputTodo} onChange={handleInputTodo} placeholder="akun GitHub.."></input>
             <button type="submit">Add</button>
         </form>
     )
--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -1,19 +1,25 @@
 import './Todo.css'
+import { useState } from "react";
 import TodoList from "../todo-list/TodoList";
 import TodoCreate from "../todo-create/TodoCreate";
 
 const Todo = () => {
-    const data =  [
+    const [getTodos, setTodos ] =  useState ([
         { id: 1, title: 'Link 1'},
-        { id: 2, title: 'Link 2'},
-        { id: 3, title: 'Link 3'},
-    ]
+    ])
+
+    const eventCreateTodo = (todo) => {
+        setTodos(getTodos.concat(todo))
+        console.log(getTodos)
+    }
+
+    
 
     return (
         <div>
             <h3>List repository</h3>
-            <TodoCreate />
-            <TodoList data={data}/>
+            <TodoCreate onCrateTodo={eventCreateTodo}/>
+            <TodoList data={getTodos}/>
         </div>
     )
 }
